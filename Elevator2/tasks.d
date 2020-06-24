759d34607abcc2e61f706646b680c57c62210613
Vivek Vivian <30543557+vivekvivian1294@users.noreply.github.com>
2020-06-24 14:51:02 +0200

    Add files via upload

---
+++ Elevator2/tasks.d
@@ -0,0 +1,14 @@
+.\tasks.o: FreeRTOS\src\tasks.c
+.\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\tasks.o: .\FreeRTOS\inc\FreeRTOS.h
+.\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\tasks.o: .\FreeRTOS\inc\projdefs.h
+.\tasks.o: .\FreeRTOSConfig.h
+.\tasks.o: .\FreeRTOS\inc\portable.h
+.\tasks.o: .\FreeRTOS\inc\portmacro.h
+.\tasks.o: .\FreeRTOS\inc\mpu_wrappers.h
+.\tasks.o: .\FreeRTOS\inc\task.h
+.\tasks.o: .\FreeRTOS\inc\list.h
+.\tasks.o: .\FreeRTOS\inc\StackMacros.h
